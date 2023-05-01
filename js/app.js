887e257d04772e80353fb07b77fddc4244c83da4
Lena <lena.sterz98@gmail.com>
2023-05-01 11:10:13 +0200

    Add method to display progress and change color when calorieLimit is reached

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ class CalorieTracker {
 		this._displayCaloriesConsumed();
 		this._displayCaloriesBurned();
 		this._displayCaloriesRemaining();
+		this._displayCaloriesProgress();
 	}
 
 	// Public Methods/API //
@@ -60,10 +61,39 @@ class CalorieTracker {
 
 	_displayCaloriesRemaining() {
 		const caloriesRemainingEl = document.querySelector("#calories-remaining");
+		const progressEl = document.querySelector("#calorie-progress");
 
 		const remaining = this._calorieLimit - this._totalCalories;
 
 		caloriesRemainingEl.innerHTML = remaining;
+
+		if (remaining < 0) {
+			caloriesRemainingEl.parentElement.parentElement.classList.remove(
+				"bg-light"
+			);
+			caloriesRemainingEl.parentElement.parentElement.classList.add(
+				"bg-danger"
+			);
+			progressEl.classList.remove("bg-success");
+			progressEl.classList.add("bg-danger");
+		} else {
+			caloriesRemainingEl.parentElement.parentElement.classList.remove(
+				"bg-danger"
+			);
+			caloriesRemainingEl.parentElement.parentElement.classList.add("bg-light");
+			progressEl.classList.remove("bg-danger");
+			progressEl.classList.add("bg-success");
+		}
+	}
+
+	_displayCaloriesProgress() {
+		const progressEl = document.querySelector("#calorie-progress");
+
+		const percentage = (this._totalCalories / this._calorieLimit) * 100;
+
+		const width = Math.min(percentage, 100);
+
+		progressEl.style.width = `${width}%`;
 	}
 
 	_render() {
@@ -71,6 +101,7 @@ class CalorieTracker {
 		this._displayCaloriesConsumed();
 		this._displayCaloriesBurned();
 		this._displayCaloriesRemaining();
+		this._displayCaloriesProgress();
 	}
 }
 
